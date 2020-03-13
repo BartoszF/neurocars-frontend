e2e264cabf895cbcc63b7bb6e0beb7c4f4618e85
BartoszF <felis.bartosz@gmail.com>
2020-03-13 19:05:30 +0100

    Mapping simulation state.

--- src/components/simulation/SimulationView.jsx
+++ src/components/simulation/SimulationView.jsx
@@ -1,22 +1,50 @@
 import React from 'react';
-import { observer } from "mobx-react";
+import { observer } from 'mobx-react';
 import useStores from '../../useStores';
 import { useHistory } from 'react-router-dom';
 import { Button, Row } from 'antd';
+import { useIntl } from 'react-intl';
+import { SimulationMessages } from '../../i18n/globalMessages/Simulation';
 
 export const SimulationView = observer(props => {
-    const { gameStore } = useStores();
-    const history = useHistory();
+  const { gameStore } = useStores();
+  const history = useHistory();
+  const intl = useIntl();
 
-    let onClick = () => {
-        gameStore.setSimulation(props.simulation);
-        history.push("/gameTest");
+  let onClick = () => {
+    gameStore.setSimulation(props.simulation);
+    history.push('/gameTest');
+  };
+
+  let getMessage = () => {
+    switch (props.simulation.state) {
+      case 'IN_CREATION':
+        return SimulationMessages.inCreationState;
+      case 'AWAITING_DATA':
+        return SimulationMessages.awaitingDataState;
+      case 'GATHERED_DATA':
+        return SimulationMessages.gatheredDataState;
+      case 'LOST_DATA':
+        return SimulationMessages.lostDataState;
+      case 'AWAITING_MODEL':
+        return SimulationMessages.awaitingModelState;
+      case 'FINISHED':
+        return SimulationMessages.finishedState;
+      default:
+        return SimulationMessages.unknownState;
     }
+  };
 
-  return <div>
-      <Row><h2>Simulation name here</h2></Row>
-      <Row><span>{`State: stateHere`}</span></Row>
+  return (
+    <div>
+      <Row>
+        <h2>Simulation name here</h2>
+      </Row>
+      <Row>
+        <span>{`${intl.formatMessage(SimulationMessages.state)}: ${intl.formatMessage(getMessage())}`}</span>
+      </Row>
 
       <Button onClick={onClick}>Learn</Button>
-  </div>;
+    </div>
+  );
 });
---
+++ src/i18n/globalMessages/Simulation.jsx
@@ -0,0 +1,44 @@
+import { defineMessages } from 'react-intl.macro';
+
+export const SimulationMessages = defineMessages({
+  state: {
+    id: 'simulation.state',
+    defaultMessage: 'State',
+    description: ''
+  },
+  inCreationState: {
+    id: 'simulation.state.IN_CREATION',
+    defaultMessage: 'Creating.',
+    description: ''
+  },
+  awaitingDataState: {
+    id: 'simulation.state.AWAITING_DATA',
+    defaultMessage: 'Waiting for data.',
+    description: ''
+  },
+  gatheredDataState: {
+    id: 'simulation.state.GATHERED_DATA',
+    defaultMessage: 'Data ready. Waiting for training.',
+    description: ''
+  },
+  lostDataState: {
+    id: 'simulation.state.LOST_DATA',
+    defaultMessage: 'Data corrupted. Please learn again.',
+    description: ''
+  },
+  awaitingModelState: {
+    id: 'simulation.state.AWAITING_MODEL',
+    defaultMessage: 'Training...',
+    description: ''
+  },
+  finishedState: {
+    id: 'simulation.state.FINISHED',
+    defaultMessage: 'Model ready.',
+    description: ''
+  },
+  unknownState: {
+    id: 'simulation.state.UNKNOWN',
+    defaultMessage: "Unknown (shouldn't happen)",
+    description: ''
+  }
+});
