bc90d2eb0f63b2b2c74c3d0e95fd25ec26911bfa
BartoszF <felis.bartosz@gmail.com>
2020-04-05 20:40:48 +0200

    Working curve editing! Working on track preview

--- src/pages/EditorPage/EditorScene.jsx
+++ src/pages/EditorPage/EditorScene.jsx
@@ -1,71 +1,12 @@
 import Phaser from 'phaser';
 import * as dat from 'dat.gui';
 import createCar from '../GamePage/Gameplay/CarFactory';
+import { PathEditor } from './editor/PathEditor';
 
 const CAMERA_SPEED = 250.0;
 
-const Vector2 = Phaser.Math.Vector2;
-
-const getParamsFromVector2 = (vector, name) => {
-  return [
-    [vector, 'x', name + ' x'],
-    [vector, 'y', name + ' y']
-  ];
-};
-
-class CurveEditor {
-  constructor(scene, graphics) {
-    this.curves = [];
-    this.listeners = [];
-    this.graphics = graphics;
-    this.path = new scene.add.path(0, 0);
-    this.path.autoClose = true;
-  }
-
-  addCurveListener(listener) {
-    this.listeners.push(listener);
-  }
-
-  addCubicBezier() {
-    console.log('ADDED C BEZIER');
-    this.path.cubicBezierTo(
-      new Vector2(100, 100),
-      new Vector2(75, 75),
-      new Vector2(50, 50)
-    );
-
-    let bezier = this.path.curves[this.path.curves.length - 1];
-
-    let params = [];
-
-    params.push(...getParamsFromVector2(bezier.p0, 'p0'));
-    params.push(...getParamsFromVector2(bezier.p1, 'p1'));
-    params.push(...getParamsFromVector2(bezier.p2, 'p2'));
-    params.push(...getParamsFromVector2(bezier.p3, 'p3'));
-
-    this.onCurveAdded('Bezier ' + this.path.curves.length, params);
-
-    this.draw();
-  }
-
-  draw() {
-    for(let i in this.path.curves) {
-      this.path.curves[i].updateArcLengths();
-    }
-    this.path.updateArcLengths();
-    console.log('draw', this.path.curves, this.path.getLength());
-    this.graphics.clear();
-    this.path.draw(this.graphics);
-    this.graphics.strokePath();
-  }
-
-  onCurveAdded(name, curve) {
-    for (var i in this.listeners) this.listeners[i](name, curve);
-  }
-}
-
 export const scene = {
-  preload: function() {
+  preload: function () {
     this.load.image('wheel', '/assets/wheel.png');
     this.load.image('car', '/assets/cars/Audi.png');
 
@@ -73,7 +14,7 @@ export const scene = {
     this.guidePosition = new Phaser.Math.Vector2(0, 0);
   },
 
-  create: function() {
+  create: function () {
     this.matter.world.setBounds(-2000, -2000, 4000, 4000).disableGravity();
     let debug = this.matter.world.debugConfig;
     debug.staticLineColor = 0x00ff00;
@@ -95,35 +36,30 @@ export const scene = {
       this
     );
 
-    let graph = this.add.graphics(0,0);
+    let graph = this.add.graphics(0, 0);
     graph.lineStyle(8, 0xffffff, 1.0);
 
-    this.editor = new CurveEditor(this, graph);
+    this.editor = new PathEditor(this, graph);
 
     var gui = new dat.GUI({ autoPlace: false });
 
-    let createFolder = gui.addFolder("Create")
+    let createFolder = gui.addFolder('Create');
 
     createFolder.add(this.editor, 'addCubicBezier').name('Add cubic bezier');
-
     createFolder.open();
 
     let editor = this.editor;
 
-    this.editor.addCurveListener((name, curve) => {
-      console.log(curve);
-
+    editor.addCurveListener((name, curve) => {
       let folder = gui.addFolder(name);
 
-      for (let i in curve) {
-        let param = curve[i];
-        let controller = folder.add(param[0], param[1]).name(param[2]);
-        controller.onFinishChange(function(value) {
-          console.log(param[0], param[0][param[1]], value);
+      console.log(name, curve);
+      let points = curve.editor.points;
+      console.log(points);
 
-          param[0][param[1]] = value;
-          editor.draw();
-        });
+      for(let i in points) {
+        let pFolder = folder.addFolder("Point " + i)
+        pFolder.add(points[i], "width");
       }
     });
 
@@ -131,7 +67,7 @@ export const scene = {
     customContainer.appendChild(gui.domElement);
   },
 
-  update: function(time, delta) {
+  update: function (time, delta) {
     const properDelta = delta / 60;
     let pointer = this.input.activePointer;
     let mousePosition = new Phaser.Math.Vector2(pointer.worldX, pointer.worldY);
@@ -153,8 +89,4 @@ export const scene = {
       pointer.worldY
     );
   },
-
-  addCubicBezier: function() {
-    console.log('ADDED');
-  }
 };
---
+++ src/pages/EditorPage/editor/CurveEditor.jsx
@@ -0,0 +1,30 @@
+import Phaser from 'phaser';
+import { Point } from './Point';
+
+const Vector2 = Phaser.Math.Vector2;
+
+export class CurveEditor {
+  constructor(pathEditor, curve, previousCurve) {
+    this.pathEditor = pathEditor;
+    this.scene = pathEditor.scene;
+    this.curve = curve;
+    this.previousCurve = previousCurve;
+    this.points = [];
+
+    //FOR NOW ITS ONLY BEZIER
+
+    if (this.previousCurve != null) {
+      this.curve.p0 = this.previousCurve.p3;
+    } else {
+      this.points.push(new Point(this, this.curve, 'p0'));
+    }
+
+    this.points.push(new Point(this, this.curve, 'p1'));
+    this.points.push(new Point(this, this.curve, 'p2'));
+    this.points.push(new Point(this, this.curve, 'p3'));
+  }
+
+  onUpdate() {
+    this.pathEditor.draw();
+  }
+}
