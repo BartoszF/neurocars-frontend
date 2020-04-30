c4c0d95f656039a124195621d173f2c086b5e9e5
BartoszF <felis.bartosz@gmail.com>
2020-04-30 22:59:27 +0200

    Fixed loading

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -20,7 +20,11 @@ const ProgressDiv = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
+  z-index: 10000;
   transform: translate(-50%, -50%);
+  & .ant-progress-text {
+    color: white;
+  }
 `;
 
 export const GamePage = observer((props) => {
@@ -57,11 +61,11 @@ export const GamePage = observer((props) => {
 
   return (
     <GameWrapper id="gameParent">
-      {loaded == false && (
+      {/*loaded == false && (*/
         <ProgressDiv>
-          <Progress type="circle" percent={progress * 100} />
+          <Progress type="circle" percent={parseInt(progress * 100)} />
         </ProgressDiv>
-      )}
+      /*)*/}
       <Unity width="100%" height="100%" unityContent={unityContent} />
     </GameWrapper>
   );
