3a5e73e40fb41fc6250bb00488ad1315d9dcfe51
BartoszF <felis.bartosz@gmail.com>
2020-05-12 17:26:24 +0200

    Fixed problems in communication. Cleaning.

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { observer } from "mobx-react";
 import useStores from "../../useStores";
-import { useHistory, Redirect } from "react-router-dom";
+import { useHistory, useParams, Redirect } from "react-router-dom";
 import { useEffect } from "react";
+import { ACCESS_TOKEN } from "../../constants";
 
 import { Progress } from "antd";
 
 import Unity, { UnityContent } from "react-unity-webgl";
+import { GameContainer } from "./GameContainer";
 
 const GameWrapper = styled.div`
   height: 100vh;
@@ -27,19 +29,20 @@ const ProgressDiv = styled.div`
   }
 `;
 
+let unityContent = new UnityContent(
+  "/game/Build.json",
+  "/game/UnityLoader.js",
+  {
+    adjustOnWindowResize: true,
+  }
+);
+
 export const GamePage = observer((props) => {
   let [progress, setProgress] = useState(0.0);
   let [loaded, setLoaded] = useState(false);
-  let { gameStore } = useStores();
-  const history = useHistory();
-
-  let unityContent = new UnityContent(
-    "/game/Build.json",
-    "/game/UnityLoader.js",
-    {
-      adjustOnWindowResize: true,
-    }
-  );
+  //let { gameStore } = useStores();
+  //const history = useHistory();
+  let { simId } = useParams();
 
   unityContent.on("progress", (progress) => {
     setProgress(progress);
@@ -47,26 +50,33 @@ export const GamePage = observer((props) => {
 
   unityContent.on("loaded", () => {
     setLoaded(true);
+    //console.log(simId);
   });
 
   unityContent.on("quitted", () => {
     console.log("QUITTING");
   });
 
+  unityContent.on("GameLoaded", () => {
+    let token = localStorage.getItem(ACCESS_TOKEN);
+    unityContent.send("Communication", "SetToken", token);
+    unityContent.send("Communication", "CommunicationReady", Number(simId));
+  });
+
   unityContent.on("Finished", (numFrames) => {
     console.log("Finished");
-    gameStore.endSimulation(numFrames);
-    history.push(`/`);
+    //gameStore.endSimulation(numFrames);
+    //history.push(`/`);
   });
 
   return (
     <GameWrapper id="gameParent">
-      {loaded == false && (
+      {loaded === false && (
         <ProgressDiv>
           <Progress type="circle" percent={parseInt(progress * 100)} />
         </ProgressDiv>
       )}
-      <Unity width="100%" height="100%" unityContent={unityContent} />
+      <GameContainer unityContent={unityContent} />
     </GameWrapper>
   );
 });
